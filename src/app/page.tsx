eccf189b2495c313c4c66f5323ea336132dd091e
Davina Leong <davina.leong.sy@gmail.com>
2024-07-29 11:14:35 +0800

    Setup Cloudinary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
-import Image from "next/image"
+import { CldImage } from "next-cloudinary"
 
 export default function Home() {
-  return <main></main>
+  return (
+    <main>
+      <CldImage
+        src="0086_r9pfpq" // Use this sample image or upload your own via the Media Explorer
+        width="501" // Transform the image: auto-crop to square aspect_ratio
+        height="668"
+        alt="Photo of Casper"
+        crop={{
+          type: "auto",
+          source: true,
+        }}
+      />
+    </main>
+  )
 }

9cbdc8d138721d2e7bc072c29056b82d7fb71bf8
Davina Leong <davina.leong.sy@gmail.com>
2024-07-29 11:54:42 +0800

    Added logo and button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,7 @@
 import type { Metadata } from "next"
+import Image from "next/image"
+import Link from "next/link"
+import logo from "./../assets/images/logo-svg.svg"
 import "./globals.scss"
 
 export const metadata: Metadata = {
@@ -13,16 +16,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="text-stone-500 first:bg-stone-50">
-        <header className="container mb-4">
-          <h1 className="font-heading text-4xl font-bold text-center">
-            Casper.js
-          </h1>
+      <body className="text-stone-500 bg-stone-50 dark:bg-stone-950 | flow">
+        <header className="container">
+          <Link href="/" className="flex gap-4 justify-center">
+            <Image src={logo.src} alt="Casper.js logo" width="64" height="64" />
+            <h1 className="font-heading text-4xl font-bold text-center">
+              Casper.js
+            </h1>
+          </Link>
         </header>
 
-        <div className="container">{children}</div>
+        <div className="container | flow">{children}</div>
 
-        <footer className="container mt-4">
+        <footer className="container">
           <p className="text-center text-sm">
             casper.js &copy; Davina Leong, 2024
           </p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,9 @@ import { CldImage } from "next-cloudinary"
 
 export default function Home() {
   return (
-    <main>
+    <main className="flow">
       <CldImage
+        className="block mx-auto rounded-sm shadow-lg"
         src="0086_r9pfpq" // Use this sample image or upload your own via the Media Explorer
         width="501" // Transform the image: auto-crop to square aspect_ratio
         height="668"
@@ -15,6 +16,12 @@ export default function Home() {
           source: true,
         }}
       />
+
+      <div className="text-center mt-4">
+        <button type="button" className="btn">
+          Get a new Dog Picture
+        </button>
+      </div>
     </main>
   )
 }

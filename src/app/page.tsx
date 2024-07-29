aa500b41d846f003712dbe244e5812d45ffbcaf5
Davina Leong <davina.leong.sy@gmail.com>
2024-07-29 18:34:56 +0800

    WIP image array

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
+"use client"
+
+import { CldImage } from "next-cloudinary"
+
 export default function Home() {
   return (
     <main className="flow">
+      <CldImage
+        className="mx-auto rounded-sm shadow-md"
+        src="0085_jscw28"
+        width="667"
+        height="501"
+        alt="Photo of Casper"
+        crop={{
+          type: "auto",
+          source: true,
+        }}
+      />
+
       <div className="text-center mt-4">
         <button type="button" className="btn">
           Get a new Dog Picture
---
+++ src/images.tsx
@@ -0,0 +1,236 @@
+import { CldImage } from "next-cloudinary"
+
+let key: number = 0
+const className: string = "mx-auto rounded-sm shadow-md"
+const images: JSX.Element[] = [
+  <CldImage
+    key={key}
+    className={className}
+    src="0086_r9pfpq"
+    width="667"
+    height="501"
+    alt="Photo of Casper"
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0085_jscw28"
+    width="667"
+    height="501"
+    alt="Photo of Casper"
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0090_doqa10"
+    width="667"
+    height="501"
+    alt="Photo of Casper"
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0089_f9gwdm"
+    width="667"
+    height="501"
+    alt="Photo of Casper"
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0088_ereruy"
+    width="667"
+    height="501"
+    alt="Photo of Casper"
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0087_z5o0li"
+    width="667"
+    height="501"
+    alt="Photo of Casper"
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0084_fuhbii"
+    width="667"
+    height="501"
+    alt="Photo of Casper"
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0080_h88vyb"
+    width="667"
+    height="501"
+    alt="Photo of Casper"
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0082_pvk8zn"
+    width="667"
+    height="501"
+    alt="Photo of Casper"
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0079_vfocs2"
+    width="667"
+    height="501"
+    alt="Photo of Casper"
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0083_osyzkq"
+    width="667"
+    height="501"
+    alt="Photo of Casper"
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0081_vdwugb"
+    width="667"
+    height="501"
+    alt="Photo of Casper"
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0078_lowszn"
+    width="667"
+    height="501"
+    alt="Photo of Casper"
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0077_ohf5fc"
+    width="667"
+    height="501"
+    alt="Photo of Casper"
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0076_ubtdms"
+    width="667"
+    height="501"
+    alt="Photo of Casper"
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0073_jdrpu5"
+    width="667"
+    height="501"
+    alt="Photo of Casper"
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0075_pwfxvk"
+    width="667"
+    height="501"
+    alt="Photo of Casper"
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0074_neytpz"
+    width="667"
+    height="501"
+    alt="Photo of Casper"
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0071_xn3pnc"
+    width="667"
+    height="501"
+    alt="Photo of Casper"
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+]
+
+export default images

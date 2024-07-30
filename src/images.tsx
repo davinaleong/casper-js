e38856759319e16fa4c22b93c0eecff32eb4b7f9
Davina Leong <davina.leong.sy@gmail.com>
2024-07-30 18:54:29 +0800

    Done with images array

--- src/images.tsx
+++ src/images.tsx
@@ -1,15 +1,16 @@
 import { CldImage } from "next-cloudinary"
 
 let key: number = 0
-const className: string = "mx-auto rounded-sm shadow-md"
+const className: string = `mx-auto rounded-sm shadow-md`
+const alt: string = `Photo of Casper`
 const images: JSX.Element[] = [
   <CldImage
     key={key}
     className={className}
     src="0086_r9pfpq"
-    width="667"
-    height="501"
-    alt="Photo of Casper"
+    width="501"
+    height="667"
+    alt={alt}
     crop={{
       type: "auto",
       source: true,
@@ -21,7 +22,7 @@ const images: JSX.Element[] = [
     src="0085_jscw28"
     width="667"
     height="501"
-    alt="Photo of Casper"
+    alt={alt}
     crop={{
       type: "auto",
       source: true,
@@ -31,9 +32,9 @@ const images: JSX.Element[] = [
     key={key++}
     className={className}
     src="0090_doqa10"
-    width="667"
-    height="501"
-    alt="Photo of Casper"
+    width="501"
+    height="667"
+    alt={alt}
     crop={{
       type: "auto",
       source: true,
@@ -43,9 +44,9 @@ const images: JSX.Element[] = [
     key={key++}
     className={className}
     src="0089_f9gwdm"
-    width="667"
-    height="501"
-    alt="Photo of Casper"
+    width="501"
+    height="667"
+    alt={alt}
     crop={{
       type: "auto",
       source: true,
@@ -57,7 +58,7 @@ const images: JSX.Element[] = [
     src="0088_ereruy"
     width="667"
     height="501"
-    alt="Photo of Casper"
+    alt={alt}
     crop={{
       type: "auto",
       source: true,
@@ -67,9 +68,9 @@ const images: JSX.Element[] = [
     key={key++}
     className={className}
     src="0087_z5o0li"
-    width="667"
-    height="501"
-    alt="Photo of Casper"
+    width="501"
+    height="667"
+    alt={alt}
     crop={{
       type: "auto",
       source: true,
@@ -81,7 +82,7 @@ const images: JSX.Element[] = [
     src="0084_fuhbii"
     width="667"
     height="501"
-    alt="Photo of Casper"
+    alt={alt}
     crop={{
       type: "auto",
       source: true,
@@ -91,9 +92,9 @@ const images: JSX.Element[] = [
     key={key++}
     className={className}
     src="0080_h88vyb"
-    width="667"
-    height="501"
-    alt="Photo of Casper"
+    width="501"
+    height="667"
+    alt={alt}
     crop={{
       type: "auto",
       source: true,
@@ -103,9 +104,9 @@ const images: JSX.Element[] = [
     key={key++}
     className={className}
     src="0082_pvk8zn"
-    width="667"
-    height="501"
-    alt="Photo of Casper"
+    width="501"
+    height="667"
+    alt={alt}
     crop={{
       type: "auto",
       source: true,
@@ -115,9 +116,9 @@ const images: JSX.Element[] = [
     key={key++}
     className={className}
     src="0079_vfocs2"
-    width="667"
-    height="501"
-    alt="Photo of Casper"
+    width="501"
+    height="667"
+    alt={alt}
     crop={{
       type: "auto",
       source: true,
@@ -127,9 +128,9 @@ const images: JSX.Element[] = [
     key={key++}
     className={className}
     src="0083_osyzkq"
-    width="667"
-    height="501"
-    alt="Photo of Casper"
+    width="501"
+    height="667"
+    alt={alt}
     crop={{
       type: "auto",
       source: true,
@@ -139,9 +140,9 @@ const images: JSX.Element[] = [
     key={key++}
     className={className}
     src="0081_vdwugb"
-    width="667"
-    height="501"
-    alt="Photo of Casper"
+    width="501"
+    height="667"
+    alt={alt}
     crop={{
       type: "auto",
       source: true,
@@ -151,9 +152,9 @@ const images: JSX.Element[] = [
     key={key++}
     className={className}
     src="0078_lowszn"
-    width="667"
-    height="501"
-    alt="Photo of Casper"
+    width="501"
+    height="667"
+    alt={alt}
     crop={{
       type: "auto",
       source: true,
@@ -165,7 +166,7 @@ const images: JSX.Element[] = [
     src="0077_ohf5fc"
     width="667"
     height="501"
-    alt="Photo of Casper"
+    alt={alt}
     crop={{
       type: "auto",
       source: true,
@@ -175,9 +176,9 @@ const images: JSX.Element[] = [
     key={key++}
     className={className}
     src="0076_ubtdms"
-    width="667"
-    height="501"
-    alt="Photo of Casper"
+    width="501"
+    height="667"
+    alt={alt}
     crop={{
       type: "auto",
       source: true,
@@ -187,9 +188,9 @@ const images: JSX.Element[] = [
     key={key++}
     className={className}
     src="0073_jdrpu5"
-    width="667"
-    height="501"
-    alt="Photo of Casper"
+    width="501"
+    height="667"
+    alt={alt}
     crop={{
       type: "auto",
       source: true,
@@ -201,7 +202,7 @@ const images: JSX.Element[] = [
     src="0075_pwfxvk"
     width="667"
     height="501"
-    alt="Photo of Casper"
+    alt={alt}
     crop={{
       type: "auto",
       source: true,
@@ -211,9 +212,9 @@ const images: JSX.Element[] = [
     key={key++}
     className={className}
     src="0074_neytpz"
-    width="667"
-    height="501"
-    alt="Photo of Casper"
+    width="501"
+    height="667"
+    alt={alt}
     crop={{
       type: "auto",
       source: true,
@@ -223,9 +224,849 @@ const images: JSX.Element[] = [
     key={key++}
     className={className}
     src="0071_xn3pnc"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0072_bfi5k2"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0068_blyvwa"
     width="667"
     height="501"
-    alt="Photo of Casper"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0070_lnnagf"
+    width="667"
+    height="501"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0067_tnv4ot"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0065_nt1xjs"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0066_ebplz6"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0069_yt0ti2"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0064_nd5v76"
+    width="667"
+    height="501"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0063_ivzvt5"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0060_kbwou8"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0059_f66fyv"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0061_ullyha"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0062_u0rp7c"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0058_ftvrdv"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0057_hec95u"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0056_ch4kw9"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0050_p1xkx8"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0055_ihszdb"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0054_byzsmx"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0051_lktqni"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0048_m03yx1"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0053_e0f6vi"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0052_kt3wu4"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0047_anhiph"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0045_okeqds"
+    width="667"
+    height="501"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0049_nqxou9"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0044_vkkt3z"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0042_yf5yqj"
+    width="667"
+    height="501"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0043_g5pmwj"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0041_dmqmxj"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0039_prcphl"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0037_jgdagt"
+    width="667"
+    height="501"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0036_yd4xhu"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0038_vonux5"
+    width="667"
+    height="501"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0040_xgektn"
+    width="667"
+    height="501"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0031_durcba"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0035_fv3afo"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0033_d2a0xp"
+    width="667"
+    height="501"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0030_n9v2zg"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0034_qltfcu"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0025_i7aozj"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0029_aqmhlv"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0028_g5o3th"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0026_eltju6"
+    width="667"
+    height="501"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0027_j8i4re"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0021_oiutok"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0024_blmdob"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0022_ukzzor"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0020_moi290"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0023_tjq8io"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0019_coms2w"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0017_v1nad2"
+    width="667"
+    height="501"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0012_gl7vm7"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0016_kkbxag"
+    width="667"
+    height="501"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0015_cfnvvz"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0018_cw5wyd"
+    width="667"
+    height="501"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0002_tssfi0"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0014_vhuywu"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0013_pvdjdw"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0008_lwhr7k"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0009_qygdp4"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0011_pw6zbz"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0006_obhusb"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0010_cw5jt0"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0007_w5skkn"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0005_ggejpy"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0004_rpuaxf"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0003_ozye9o"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0091_p1vom6"
+    width="501"
+    height="667"
+    alt={alt}
+    crop={{
+      type: "auto",
+      source: true,
+    }}
+  />,
+  <CldImage
+    key={key++}
+    className={className}
+    src="0001_rhxod7"
+    width="501"
+    height="667"
+    alt={alt}
     crop={{
       type: "auto",
       source: true,

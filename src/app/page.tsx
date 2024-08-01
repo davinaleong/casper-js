48fefc02f70a7f127e3a1ede3a33859358ec5f6c
Davina Leong <davina.leong.sy@gmail.com>
2024-08-01 11:56:37 +0800

    Added TODO comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,13 @@ import { getRandomElement } from "@/lib/helpers"
 
 const image: any = getRandomElement(images)
 
+/**
+ * TODO:
+ * 1. Use useState to store image
+ * 2. On button click, change image
+ * 3. Display image
+ */
+
 export default function Home() {
   return (
     <main className="flow">

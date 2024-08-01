08cc8142450a3ffd9159e87af29f24506f8c2b53
Davina Leong <davina.leong.sy@gmail.com>
2024-08-01 12:12:47 +0800

    Added a button to get another photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,31 @@
 "use client"
-
+import { useState } from "react"
 import images from "@/images"
 import { getRandomElement } from "@/lib/helpers"
 
-const image: any = getRandomElement(images)
-
-/**
- * TODO:
- * 1. Use useState to store image
- * 2. On button click, change image
- * 3. Display image
- */
+const thisImage: any = getRandomElement(images)
 
 export default function Home() {
+  const [image, setImage] = useState(thisImage)
+
+  const buttonClickHandler = () => {
+    const thisImage: any = getRandomElement(images)
+    setImage(thisImage)
+  }
+
   return (
     <main className="flow">
       {image}
+
+      <div className="text-center">
+        <button
+          className="btn font-heading"
+          type="button"
+          onClick={buttonClickHandler}
+        >
+          Get another Dog photo
+        </button>
+      </div>
 
       <p className="text-center text-lg">
         Refresh the page to get a new dog photo.
